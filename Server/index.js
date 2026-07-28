0128915e17a1774cbe7809d18a1141b1fd213a2e
SakshiCode731 <sakshigautam90111@gmail.com>
2026-07-29 00:04:19 +0530

    thre is updating with index file

--- Server/index.js
+++ Server/index.js
@@ -25,6 +25,7 @@ app.use('/api/resume', resumeRoutes);
 app.use('/api/readiness', readinessRoutes);
 app.use('/api/mock-interview', mockInterviewRoutes);
 app.use('/api/answer', answerEvaluatorRoutes);
+app.use('/api/progress', require('./routes/progressRoutes'));
 app.use('/api/chat', chatRoutes);
 app.use('/api/admin', require('./routes/adminRoutes'));
 
