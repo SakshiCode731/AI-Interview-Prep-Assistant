a4b087fb548bd86d020e17bb17f84efc51d580e1
SakshiCode731 <sakshigautam90111@gmail.com>
2026-07-30 23:16:55 +0530

    there is adding some code here

--- Server/routes/progressRoutes.js
+++ Server/routes/progressRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { getProgress } = require('../controllers/progressController');
 const { protect } = require('../middleware/authMiddleware');
 // @desc Get user progress
+// @route GET /api/progress
 
 router.get('/', protect, getProgress);
 
