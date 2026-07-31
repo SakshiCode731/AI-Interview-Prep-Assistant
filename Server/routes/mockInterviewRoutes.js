2d0d4ba7942329020d8f61bdce1be2b5a7dc84a2
SakshiCode731 <sakshigautam90111@gmail.com>
2026-07-31 22:53:41 +0530

    there is adding some code

--- Server/routes/mockInterviewRoutes.js
+++ Server/routes/mockInterviewRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getMockQuestions, evaluateAnswer } = require('../controllers/mockInterviewController');
 const { protect } = require('../middleware/authMiddleware');
+// this is interview portion
 
 router.post('/questions', protect, getMockQuestions);
 router.post('/evaluate', protect, evaluateAnswer);
